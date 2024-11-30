7c686aef41181b1f117878e60002bd228d7aede6
AdamD-NZ <adam.donson@gmail.com>
2024-11-30 21:47:07 +0000

    This commit to the GitHub repository includes changes to both the Cell and Player java classes. The changes to the Player class included additional commentary around each Enum value. The change to the Cell class was to the Constructor to initialise a cell at a specific position with an initial content.

--- BIT504_Programming_1_Assessment2_Raw/src/Package/Player.java
+++ BIT504_Programming_1_Assessment2_Raw/src/Package/Player.java
@@ -8,9 +8,34 @@
 package Package;
 
 /**
- * Enumeration for the players move
+ * Enumeration for the players' move.
+ * 
+ * This enum is used to represent the possible states of a cell in the game board.
+ * It defines three possible values for a cell:
+ * - Empty: Represents an empty cell that has not been filled yet.
+ * - Cross: Represents a cell that has been filled with a "X" symbol.
+ * - Nought: Represents a cell that has been filled with a "O" symbol.
+ * 
+ * The enum helps to track the state of each cell and determine the game progress (whether a player has won, or if the game is a draw).
  */
 
 public enum Player {
-	Empty, Cross, Nought
+	/**
+     * Represents an empty cell on the board.
+     * Initially, all cells are empty before any player makes a move.
+     */
+	Empty,
+	
+	/**
+     * Represents a move made by the first player (usually 'X').
+     * This value is used to indicate that a cell has been filled with a "X" symbol.
+     */
+	
+	Cross,
+	
+	/**
+     * Represents a move made by the second player (usually 'O').
+     * This value is used to indicate that a cell has been filled with an "O" symbol.
+     */
+	Nought
 }
